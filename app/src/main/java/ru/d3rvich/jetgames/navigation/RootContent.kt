6eca8df620dc3a3b864fb5367ef47bacf76e5efd
D3rvich <d3rvich@gmail.com>
2026-05-16 16:27:54 +0500

    Add animations in RootContent

--- app/src/main/java/ru/d3rvich/jetgames/navigation/RootContent.kt
+++ app/src/main/java/ru/d3rvich/jetgames/navigation/RootContent.kt
@@ -3,18 +3,32 @@ package ru.d3rvich.jetgames.navigation
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.arkivanov.decompose.FaultyDecomposeApi
 import com.arkivanov.decompose.extensions.compose.stack.Children
+import com.arkivanov.decompose.extensions.compose.stack.animation.fade
+import com.arkivanov.decompose.extensions.compose.stack.animation.slide
+import com.arkivanov.decompose.extensions.compose.stack.animation.stackAnimation
 import ru.d3rvich.filter.FilterScreen
 import ru.d3rvich.jetgames.navigation.root.RootComponent
 import ru.d3rvich.settings.SettingsScreen
 
+@OptIn(FaultyDecomposeApi::class)
 @Composable
 fun RootContent(
     rootComponent: RootComponent,
     windowSizeClass: WindowSizeClass,
     modifier: Modifier = Modifier
 ) {
-    Children(rootComponent.stack, modifier) {
+    Children(
+        rootComponent.stack,
+        modifier,
+        animation = stackAnimation { child, _, _ ->
+            when (child.instance) {
+                is RootComponent.Child.Filter, is RootComponent.Child.Settings -> slide()
+                is RootComponent.Child.GameDetail -> fade()
+                is RootComponent.Child.Main -> null
+            }
+        }) {
         when (val child = it.instance) {
             is RootComponent.Child.GameDetail -> {
                 GameDetailContent(child.component)
