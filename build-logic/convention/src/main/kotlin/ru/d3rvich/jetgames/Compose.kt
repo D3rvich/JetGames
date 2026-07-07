7c310924b600a41f1e82c84b3a7654f8fd4f8ed1
D3rvich <d3rvich@gmail.com>
2026-07-07 18:54:21 +0500

    Add compose-gradlePlugin. Add compose-stability-config. Add compose metrics

--- build-logic/convention/src/main/kotlin/ru/d3rvich/jetgames/Compose.kt
+++ build-logic/convention/src/main/kotlin/ru/d3rvich/jetgames/Compose.kt
@@ -2,8 +2,12 @@ package ru.d3rvich.jetgames
 
 import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
-import org.jetbrains.kotlin.gradle.dsl.KotlinJvmExtension
+import org.jetbrains.kotlin.compose.compiler.gradle.ComposeCompilerGradlePluginExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinBaseExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 
 internal fun Project.configureAndroidCompose(commonExtension: CommonExtension) {
     commonExtension.apply {
@@ -23,15 +27,31 @@ internal fun Project.configureAndroidCompose(commonExtension: CommonExtension) {
             "debugImplementation"(libs.findLibrary("androidx-compose-ui-tooling").get())
         }
     }
+    configureCompose<KotlinAndroidProjectExtension>()
 }
 
-internal fun Project.configureJvmCompose(commonExtension: KotlinJvmExtension) {
-    commonExtension.apply {
-        pluginManager.apply("org.jetbrains.kotlin.plugin.compose")
+internal fun Project.configureJvmCompose(kotlinJvmExtension: KotlinJvmProjectExtension) {
+    kotlinJvmExtension.apply {
         dependencies {
-            val bom = libs.findLibrary("androidx-compose-bom").get()
-            "implementation"(platform(bom))
             "implementation"(libs.findLibrary("androidx-compose-runtime").get())
+        }
+    }
+    configureCompose<KotlinJvmProjectExtension>()
+}
+
+private inline fun <reified T : KotlinBaseExtension> Project.configureCompose() = configure<T> {
+    pluginManager.apply("org.jetbrains.kotlin.plugin.compose")
+    val enableMetrics = project.providers.gradleProperty("enableComposeCompilerReports")
+        .map(String::toBoolean).getOrElse(false)
+
+    pluginManager.withPlugin("org.jetbrains.kotlin.plugin.compose") {
+        configure<ComposeCompilerGradlePluginExtension> {
+            stabilityConfigurationFiles.add(rootProject.layout.projectDirectory.file("compose_compiler_config.conf"))
+
+            if (enableMetrics) {
+                metricsDestination.set(layout.buildDirectory.dir("compose_metrics"))
+                reportsDestination.set(layout.buildDirectory.dir("compose_reports"))
+            }
         }
     }
 }
